fc3f44b87809444bb19d4ceead7dec08bbcbc836
thilakvikram11 <thilakvikram13@gmail.com>
2024-12-29 16:56:53 +0530

    updated to type script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDom from "react-dom/client";
 import "./styles.css";
-import HomePage from "./Pages/HomePage";
+import HomePage from "./HomePage"
 
 let root = ReactDom.createRoot(document.getElementById("root"));
 root.render(<HomePage/>)
